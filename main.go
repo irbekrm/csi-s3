ca189dd37f09140b143cfe53588c3d13498bccf0
Irbe Krumina <irbekrm@gmail.com>
2020-10-01 11:23:41 +0100

    Implements GetPluginInfo RPC for Identity service

--- main.go
+++ main.go
@@ -12,9 +12,18 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+const (
+	driverName = "s3.csi.irbe.dev"
+	driverRepo = "https://github.com/irbekrm/csi-s3.git"
+)
+
 func main() {
-	var csiAddress string
+	var (
+		csiAddress    string
+		driverVersion string
+	)
 	flag.StringVar(&csiAddress, "csi-address", "/csi/csi.sock", "Path of the UDS on which the gRPC server will serve Identity, Node, Controller services")
+	flag.StringVar(&driverVersion, "v", "test", "driver release version")
 	flag.Parse()
 
 	if err := os.RemoveAll(csiAddress); err != nil {
@@ -28,19 +37,28 @@ func main() {
 	defer l.Close()
 
 	s := grpc.NewServer()
-	i := identityServer{}
+	i := identityServer{driverVersion: driverVersion}
 	csi.RegisterIdentityServer(s, &i)
 	reflection.Register(s)
+
 	if err := s.Serve(l); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to run grpc server: %v", err)
 		os.Exit(1)
 	}
 }
 
-type identityServer struct{}
+type identityServer struct {
+	driverVersion string
+}
 
+// GetPluginInfo returns information about this CSI plugin
 func (s identityServer) GetPluginInfo(context.Context, *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
-	return &csi.GetPluginInfoResponse{}, nil
+	m := map[string]string{"url": driverRepo}
+	return &csi.GetPluginInfoResponse{
+		Name:          driverName,
+		VendorVersion: s.driverVersion,
+		Manifest:      m,
+	}, nil
 }
 
 func (s identityServer) GetPluginCapabilities(context.Context, *csi.GetPluginCapabilitiesRequest) (*csi.GetPluginCapabilitiesResponse, error) {
