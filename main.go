c9b28df63bf4cd3784ec9c50c3f4b5e9df93ee97
Irbe Krumina <irbekrm@gmail.com>
2020-10-01 09:50:41 +0100

    Enables gRPC server reflection
    To make it easier to test the gRPC server with tools such as grpcurl

--- main.go
+++ main.go
@@ -9,16 +9,13 @@ import (
 
 	csi "github.com/container-storage-interface/spec/lib/go/csi"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
 	var csiAddress string
 	flag.StringVar(&csiAddress, "csi-address", "/csi/csi.sock", "Path of the UDS on which the gRPC server will serve Identity, Node, Controller services")
 	flag.Parse()
-
-	s := grpc.NewServer()
-	i := identityServer{}
-	csi.RegisterIdentityServer(s, &i)
 
 	if err := os.RemoveAll(csiAddress); err != nil {
 		fmt.Fprintf(os.Stderr, "could not remove socket %s: %v", csiAddress, err)
@@ -30,6 +27,10 @@ func main() {
 	}
 	defer l.Close()
 
+	s := grpc.NewServer()
+	i := identityServer{}
+	csi.RegisterIdentityServer(s, &i)
+	reflection.Register(s)
 	if err := s.Serve(l); err != nil {
 		fmt.Fprintf(os.Stderr, "failed to run grpc server: %v", err)
 		os.Exit(1)
